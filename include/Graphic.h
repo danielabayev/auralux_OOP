20eb55d6ee5a41855afeee896e6ea6746e5c8adf
daniel abayev <75631862+danielabayev@users.noreply.github.com>
2021-06-13 09:51:06 +0300

    temp screen upload

--- include/Graphic.h
+++ include/Graphic.h
@@ -1,15 +1,18 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Texture.hpp>
-
+#include <memory>
 
 class Graphic
 {
 public:
 	static Graphic& PicturesObject();
+	sf::Texture& getTexture(int texture);
+	sf::Font& getFont();
 	~Graphic() = default;
 
 private:
 	Graphic();
-
+	std::vector<sf::Texture> m_textures;
+	sf::Font m_font;
 };
---
+++ src/Graphic.cpp
@@ -0,0 +1,30 @@
+#include "Graphic.h"
+#include "utillities.h"
+
+Graphic& Graphic::PicturesObject()
+{
+	static Graphic graphic;
+	return graphic;
+}
+
+sf::Texture& Graphic::getTexture(int texture)
+{
+	return m_textures[texture];
+}
+
+sf::Font& Graphic::getFont()
+{
+	return m_font;
+}
+
+Graphic::Graphic()
+{
+	m_textures.resize(TEXTURES);
+	m_textures[STARTBACKGROUND].loadFromFile("backgroundStartPage.png");
+	m_textures[RIGHTARROW].loadFromFile("right-arrow.png");
+	//m_textures[STARTBACKGROUND].loadFromFile("startbutton.png");
+	m_font.loadFromFile("sansation.ttf");
+/*	m_textures[STARTBUTTON].loadFromFile("startbutton.png");
+	m_textures[SPEEDBUTTON].loadFromFile("speedbutton.png");
+	m_textures[LEVELBUTTON].loadFromFile("levelbutton.png");*/
+}
